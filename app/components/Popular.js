bf61bb61b7b5aa3fea60a1af19b4f4798caadfa5
Emir AL <36728456+themythia@users.noreply.github.com>
2021-04-18 21:54:13 +0300

    added fetch

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import {fetchPopularRepos} from '../utils/api'
 
 const LanguagesNav = ({selected, onUpdateLanguage}) => {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -27,22 +28,48 @@ LanguagesNav.propTypes = {
 export default class Popular extends React.Component {
   constructor(props) {
     super(props);    
-    this.state = {selectedLanguage: 'All'};
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
+    };
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
-
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
+  }
   updateLanguage (language) {
-    this.setState({selectedLanguage: language});
+    this.setState({
+      selectedLanguage: language,
+      error: null,
+      repos: null
+    });
+    fetchPopularRepos (language)
+      .then(repos => this.setState({
+        repos: repos,
+        error: null
+      }))
+      .catch(() => {
+        console.warn('Error fetching repos: ', error);
+        this.setState({error: 'There was an error fetching the repositories.'})
+      })
   }
-
+  //if repos and error states are both null, we consider it still loading
+  isLoading() {
+    return this.state.repos === null && this.state.error === null;
+  }
   render() {
-    const {selectedLanguage} = this.state;
+    const {selectedLanguage, repos, error} = this.state;
     return (
       <React.Fragment>
         <LanguagesNav
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
+        {this.isLoading() && <p>LOADING</p>}
+        {error && <p>{error}</p>}
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </React.Fragment>
     );      
   }
