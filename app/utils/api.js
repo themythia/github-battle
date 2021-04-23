13dbd1282b5b81a98a34257d81d8a44cd0b9ab32
Emir AL <36728456+themythia@users.noreply.github.com>
2021-04-23 21:57:00 +0300

    added withhover

--- app/utils/api.js
+++ app/utils/api.js
@@ -4,53 +4,62 @@ const params = `?client_id=${id}&client_secret=${sec}`;
 
 const getErrorMsg = (message, username) => {
   if (message === 'Not Found') {
-    return `${username} doesnt exist`
+    return `${username} doesnt exist`;
   }
-}
+};
 const getProfile = (username) => {
   return fetch(`https://api.github.com/users/${username}${params}`)
-    .then(res => res.json())
-    .then(profile => {
+    .then((res) => res.json())
+    .then((profile) => {
       if (profile.message) {
-        throw new Error(getErrorMsg(profile.message, username))
+        throw new Error(getErrorMsg(profile.message, username));
       }
-      return profile
-    })
-}
+      return profile;
+    });
+};
 
 const getRepos = (username) => {
-  return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
-    .then(res => res.json())
-    .then(repos => {
+  return fetch(
+    `https://api.github.com/users/${username}/repos${params}&per_page=100`
+  )
+    .then((res) => res.json())
+    .then((repos) => {
       if (repos.message) {
-        throw new Error(getErrorMsg(repos.message, username))
+        throw new Error(getErrorMsg(repos.message, username));
       }
-      return repos
-    })
-}
+      return repos;
+    });
+};
 
 const getStarCount = (repos) => {
-  return repos.reduce((count, {stargazers_count}) => count + stargazers_count , 0)
-}
+  return repos.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
+};
 
-const calculateScore = (followers, repos) => ((followers * 3) + getStarCount(repos))
+const calculateScore = (followers, repos) =>
+  followers * 3 + getStarCount(repos);
 
 const getUserData = (player) => {
-  return Promise.all([getProfile(player), getRepos(player)])
-    .then(([profile, repos]) => ({
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
       profile,
-      score: calculateScore(profile.followers, repos)
-    }))
-}
+      score: calculateScore(profile.followers, repos),
+    })
+  );
+};
 
 const sortPlayers = (players) => {
-  return players.sort((a,b) => b.score - a.score)
-}
+  return players.sort((a, b) => b.score - a.score);
+};
 
 export const battle = (players) => {
-  return Promise.all([getUserData(players[0]), getUserData(players[1])])
-    .then(results => sortPlayers(results))
-}
+  return Promise.all([
+    getUserData(players[0]),
+    getUserData(players[1]),
+  ]).then((results) => sortPlayers(results));
+};
 
 export function fetchPopularRepos(language) {
   const endpoint = window.encodeURI(
