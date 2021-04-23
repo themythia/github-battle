3bdc09678aa8a4134d6a8602ba4a773f4b7cd524
Emir AL <36728456+themythia@users.noreply.github.com>
2021-04-23 19:37:50 +0300

    aaded tooltip.js

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -9,6 +9,7 @@ import {
 } from 'react-icons/fa';
 import Card from './Card';
 import Loading from './Loading';
+import Tooltip from './Tooltip';
 
 const LanguagesNav = ({ selected, onUpdateLanguage }) => {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -58,8 +59,10 @@ const ReposGrid = ({ repos }) => {
             >
               <ul className='card-list'>
                 <li>
-                  <FaUser color='rgb(255, 191, 116)' size={22} />
-                  <a href={`https://github.com/${login}`}>{login}</a>
+                  <Tooltip text='GitHub Username'>
+                    <FaUser color='rgb(255, 191, 116)' size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
                 </li>
                 <li>
                   <FaStar color='rgb(255, 215, 0)' size={22} />
