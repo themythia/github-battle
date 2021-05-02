916f6b93b5930029aa712960a4014b68cef6ce65
Emir AL <36728456+themythia@users.noreply.github.com>
2021-05-02 13:58:26 +0300

    splitted code

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
     ],
   },
-  mode: 'development',
+  mode: 'production',
   plugins: [
     new HtmlWebPackPlugin({
       template: 'app/index.html',
